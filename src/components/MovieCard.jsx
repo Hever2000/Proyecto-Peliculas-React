191c4d20791a3ae511fe6ec3889a446b65ff362f
hever <santicoronellancellotti@gmail.com>
2026-02-20 02:24:34 -0300

    favorites funcionando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,36 @@
 import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
-function MovieCard({movie}){
+function MovieCard({ movie }) {
+  const { isFavorite, addFavorites, removeFavorites } = useMovieContext();
 
-  function onLike(){
-    console.log("Liked movie");
+  const favorite = isFavorite(movie.id);
+
+  function onLike(e) {
+    e.preventDefault();
+    if (favorite) {
+      removeFavorites(movie.id);
+    } else {
+      addFavorites(movie);
+    }
   }
 
+  const buttonClass = `favorite-btn ${favorite ? "active" : ""}`;
 
-  return(
+  return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
-          <button className="favorite-btn" onClick={onLike}>♥</button>
+          <button
+            className={buttonClass}
+            onClick={onLike}
+          >
+            ♥
+          </button>
         </div>
       </div>
       <div className="movie-info">
@@ -20,7 +38,7 @@ function MovieCard({movie}){
         <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
-  )
+  );
 }
 
 export default MovieCard;
--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -13,6 +13,32 @@ export const MovieProvider = ({ children }) => {
     if(storedFavs){setFavorites(JSON.parse(storedFavs))}
   }, [])
 
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites))
+  }, [favorites])
 
-  return <MovieContext.Provider>{children}</MovieContext.Provider>;
+  const addFavorites = (movie) => {
+    setFavorites(prev => [...prev, movie]);
+  }
+
+  const removeFavorites = (movieId) => {
+    setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+  }
+
+  const isFavorite = (movieId) => {
+    return favorites.some(movie => movie.id === movieId);
+  }
+
+  const value = {
+    favorites,
+    addFavorites,
+    removeFavorites,
+    isFavorite
+
+  }
+
+
+  return <MovieContext.Provider value={value}>{children}</MovieContext.Provider>;
 };
+
+
