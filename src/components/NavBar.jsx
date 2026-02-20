c263e8951dd4fd4979d42de578834a4b1637c008
hever <santicoronellancellotti@gmail.com>
2026-02-20 01:08:06 -0300

    img

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router";
 import "../css/Navbar.css";
+import logo from "../assets/images/logo.png";
 
 function NavBar(){
   return(
     <nav className="navbar">
       <div className="navbar-brand">
-        <img src="../images/logo.png" alt="MovieApp Logo" className="navbar-logo" />
+        <img src={logo} alt="MovieApp Logo" className="navbar-logo" />
         <Link to="/">MovieApp</Link>
       </div>
       <div className="navbar-links">
